77ae39d8b24273a2c0567c527b43fd165cc50a36
Kyrylo <kyrylokondrashov@gmail.com>
2025-11-18 21:27:15 +0100

    study_gpt_training_list-approach

---
+++ Kirill_folder/main.py
@@ -0,0 +1,13 @@
+my_nums = [4, True, 'abc', 78, [1, 5, 7, 9]]
+
+my_nums.pop(2)
+
+print(my_nums)
+print(len(my_nums))
+# other_list = list(my_nums[-1])
+other_list = [2, 62, 4, 7, 4, 3]
+other_list.sort(reverse=True)
+print(other_list)
+print(sum(other_list))
+# print(other_list)other_list
+# print(my_nums.sort(key=None, reverse=True))
